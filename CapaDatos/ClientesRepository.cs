12aa1538dc669889a9e25c1a1c7feeac8b3f1830
IT7 Intelaf <itprogintelaf7@outlook.com>
2023-08-16 12:34:59 -0600

    actualizacion ClientesReporsitory

--- CapaDatos/ClientesRepository.cs
+++ CapaDatos/ClientesRepository.cs
@@ -17,6 +17,7 @@ namespace CRUD_BD_Clientes1.CapaDatos
         string cadenaConexion = "Data Source=192.168.220.251\\SQLTESTZEUS,51427;Initial Catalog=INTELAF TIENDA;Integrated Security=True;";
         //Prueba para hacer commit desde cmd
         //Prueba commit desde git hub
+        //Prueba commit desde local
 
         public ClientesRepository()
         {
